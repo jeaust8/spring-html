e40e36deb6f950281693ec3d243531973c36347c
jeaust8 <82814866+jeaust8@users.noreply.github.com>
2024-02-29 11:30:14 +0100

    29-02 css toegevoegd, werkt nog niet in de browser met localhost:8080

--- src/main/java/com/okayjava/html/repo/UserRepository.java
+++ src/main/java/com/okayjava/html/repo/UserRepository.java
@@ -1,7 +1,8 @@
 package com.okayjava.html.repo;
 
 import com.okayjava.html.model.User;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends CrudRepository <User, Integer> {
+
+public interface UserRepository extends JpaRepository<User, Integer> {
 }
